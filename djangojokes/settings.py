06f399503f1ff19cc55c9d65596bb93b0c7f6a5f
ss21113 <subas.shrestha@mdcourts.gov>
2024-08-20 14:18:19 -0400

    Phase1 Jokes

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Local Apps
+    'jokes.apps.JokesConfig'
+    'pages.apps.PagesConfig',
+
 ]
 
 MIDDLEWARE = [
@@ -54,7 +59,7 @@ ROOT_URLCONF = 'djangojokes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
